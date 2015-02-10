34322c910c391d2344e1010692901cbe834126af
Ali Ebrahim <aebrahim@ucsd.edu>
2015-02-10 14:47:16 -0800

    initial commit

---
+++ make.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+
+wheels = [i for i in os.listdir(".") if i.endswith(".whl")]
+
+html_line = """<a href="%s">%s</a><br>\n"""
+
+with open("index.html", "w") as outfile:
+    for w in wheels:
+        outfile.write(html_line % (w, w))
+
