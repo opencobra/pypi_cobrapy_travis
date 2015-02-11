fcc398c81c7f0f4feb57497fcb3b9454148cef79
Ali Ebrahim <aebrahim@ucsd.edu>
2015-02-10 16:00:55 -0800

    index now valid html

--- make.py
+++ make.py
@@ -8,6 +8,9 @@ wheels.sort()
 html_line = """<a href="%s">%s</a><br>\n"""
 
 with open("index.html", "w") as outfile:
+    outfile.write("<!DOCTYPE html>\n")
+    outfile.write(r"<head><title>install dependencies</title></head>" + "\n")
+    outfile.write("<body>\n")
     for w in wheels:
         outfile.write(html_line % (w, w))
-
+    outfile.write(r"</body></html>")
