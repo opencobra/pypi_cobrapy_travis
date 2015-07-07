0e01f46b5c8e7aa910a878645054fe24cf51dc52
Ali Ebrahim <aebrahim@ucsd.edu>
2015-07-07 01:30:17 -0700

    add Cython source
    This allows builds for which wheels have not yet been provided

--- make.py
+++ make.py
@@ -2,8 +2,9 @@
 
 import os
 
-wheels = [i for i in os.listdir(".") if i.endswith(".whl")]
-wheels.sort()
+installers = [i for i in os.listdir(".") if i.endswith(".whl") or
+              i.endswith(".tar.gz")]
+installers.sort()
 
 html_line = """<a href="%s">%s</a><br>\n"""
 
@@ -11,6 +12,6 @@ with open("index.html", "w") as outfile:
     outfile.write("<!DOCTYPE html>\n")
     outfile.write(r"<head><title>install dependencies</title></head>" + "\n")
     outfile.write("<body>\n")
-    for w in wheels:
+    for w in installers:
         outfile.write(html_line % (w, w))
     outfile.write(r"</body></html>")
